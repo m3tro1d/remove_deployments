53556af03a1a4393d1059ac4f47e43a8aa478728
m3tro1d <qazwf734@gmail.com>
2021-02-04 13:43:27 +0300

    Set up the script and add an argument parser

--- remove_deployments.py
+++ remove_deployments.py
@@ -1,89 +1,116 @@
-"""
-This is free and unencumbered software released into the public domain.
+# from sys import exit
+# import requests
 
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org/>
-"""
-
-from sys import exit
-import requests
-
-print("""
-                                     _ _     _
-        _____      _____ _ __       | | |__ | |__
-       / _ \ \ /\ / / _ \ '_ \ _____| | '_ \| '_ \
-      |  __/\ V  V /  __/ | | |_____| | |_) | | | |
-       \___| \_/\_/ \___|_| |_|     |_|_.__/|_| |_|
-                              QUICK & DIRTY SCRIPTS
+# print("""
+#                                      _ _     _
+#         _____      _____ _ __       | | |__ | |__
+#        / _ \ \ /\ / / _ \ '_ \ _____| | '_ \| '_ \
+#       |  __/\ V  V /  __/ | | |_____| | |_) | | | |
+#        \___| \_/\_/ \___|_| |_|     |_|_.__/|_| |_|
+#                               QUICK & DIRTY SCRIPTS
 
 
-                       ⚠ WARNING ⚠
-              PLEASE MAKE SURE YOU DISCONNECTED
-             YOUR SERVICES (GitHub, Heroku, etc.)
-                   BEFORE PROCEEDING.
+#                        ⚠ WARNING ⚠
+#               PLEASE MAKE SURE YOU DISCONNECTED
+#              YOUR SERVICES (GitHub, Heroku, etc.)
+#                    BEFORE PROCEEDING.
 
-This will _delete_ deployments from the "Deployments" tab from github,
-using the endpoints documented at:
-https://developer.github.com/v3/repos/deployments/#delete-a-deployment
+# This will _delete_ deployments from the "Deployments" tab from github,
+# using the endpoints documented at:
+# https://developer.github.com/v3/repos/deployments/#delete-a-deployment
 
-Make sure you use a personal access token that has
-`repo_deployments` authorized.
+# Make sure you use a personal access token that has
+# `repo_deployments` authorized.
 
-See https://stackoverflow.com/a/61272173/9943464 for more information.
+# See https://stackoverflow.com/a/61272173/9943464 for more information.
 
 
-Licensed under the Unlicensed license (see the beginning of the file)
-""")
+# Licensed under the Unlicensed license (see the beginning of the file)
+# """)
 
-OWNER = input('OWNER/repo => ')
-if '/' in OWNER:
-  OWNER, REPO = OWNER.split('/')
-else:
-  REPO = input('owner/REPO => ')
-TOK = input('Personal access token => ')
+# OWNER = input('OWNER/repo => ')
+# if '/' in OWNER:
+#   OWNER, REPO = OWNER.split('/')
+# else:
+#   REPO = input('owner/REPO => ')
+# TOK = input('Personal access token => ')
 
-url = lambda end: f'https://api.github.com/repos/{OWNER}/{REPO}/deployments{end}'
+# url = lambda end: f'https://api.github.com/repos/{OWNER}/{REPO}/deployments{end}'
 
-print(f'Running with base url: {url("")}')
+# print(f'Running with base url: {url("")}')
 
-# The token needs to be sent in this request too for private repos (thanks mxcl@so)
-all_ids = [ deploy['id'] for deploy in requests.get(url(''), headers={'Authorization': f'token {TOK}'}).json() ]
-if not all_ids:
-  print('No deployments found.')
-  exit(0)
-print(f'Got deployments with IDs: {all_ids}')
+# # The token needs to be sent in this request too for private repos (thanks mxcl@so)
+# all_ids = [ deploy['id'] for deploy in requests.get(url(''), headers={'Authorization': f'token {TOK}'}).json() ]
+# if not all_ids:
+#   print('No deployments found.')
+#   exit(0)
+# print(f'Got deployments with IDs: {all_ids}')
 
-for deploy_id in all_ids:
-  # Set to inactive
-  print(f'POST {url("")}{deploy_id}/statuses state=inactive')
-  requests.post(
-    url(f'/{deploy_id}/statuses'),
-    {'state': 'inactive'},
-    headers={
-      'Authorization': f'token {TOK}',
-      'Accept': 'application/vnd.github.ant-man-preview+json'
-    }
-  )
-  # Delete
-  print(f'DELETE {url("")}/{deploy_id}')
-  requests.delete(url('/'+str(deploy_id)), headers={'Authorization': f'token {TOK}'})
+# for deploy_id in all_ids:
+#   # Set to inactive
+#   print(f'POST {url("")}{deploy_id}/statuses state=inactive')
+#   requests.post(
+#     url(f'/{deploy_id}/statuses'),
+#     {'state': 'inactive'},
+#     headers={
+#       'Authorization': f'token {TOK}',
+#       'Accept': 'application/vnd.github.ant-man-preview+json'
+#     }
+#   )
+#   # Delete
+#   print(f'DELETE {url("")}/{deploy_id}')
+#   requests.delete(url('/'+str(deploy_id)), headers={'Authorization': f'token {TOK}'})
+from textwrap import dedent
+import argparse
+import sys
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Classes
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class CustomArgumentParser(argparse.ArgumentParser):
+    """Override ArgumentParser's help message"""
+    def format_help(self):
+        help_text = dedent(f"""\
+        A handy script for removing the deployments from GitHub repositories.
+
+        Usage: {self.prog} [OPTIONS] URL TOKEN
+
+        URL:
+          URL of a repository in the format of owner/repo
+
+        TOKEN
+            Your GitHub personal access token.
+            Make sure it has 'repo_deployments' authorized.
+
+        Options:
+          -h,  --help     show help
+
+        For more information visit:
+        https://github.com/m3tro1d/remove_deployments
+        """)
+        return help_text
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Functions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def parse_args():
+    """Process input arguments"""
+    parser = CustomArgumentParser(usage="%(prog)s [OPTIONS] URL TOKEN")
+    return parser.parse_args()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Main script
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def main():
+    """Main function"""
+    pass
+
+
+# Entry point
+if __name__ == "__main__":
+    args = parse_args()
+    main()
